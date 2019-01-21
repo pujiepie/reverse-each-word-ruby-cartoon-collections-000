6bf3968382838a62bcd3c70b3bf2f8bc50675ccd
Puja Patel <pujap52@gmail.com>
2019-01-21 04:54:36 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def reverse_each_word(string)
-  array = string.split(" ") 
-  test_array = []
-  array.collect do |string| 
-    test_array << string.reverse
+def reverse_each_word_with_each(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
   end
-  test_array.join(" ")
+  return_array.join(" ")
 end
